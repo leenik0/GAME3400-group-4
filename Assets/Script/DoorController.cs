8349c5ff424aaaff58d62eb762124007ed98e225
Sorano7 <sorano27@outlook.com>
2026-01-28 20:16:30 -0500

    Fix audio and remove unnecessary script; Tune lighting

--- Assets/Script/DoorController.cs
+++ Assets/Script/DoorController.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class DoorController : MonoBehaviour
 {
     [SerializeField] private float defaultSpeed = 1f;
     [SerializeField] private string openParam = "IsOpen";
     [SerializeField] private Animator animator;
-
+    [SerializeField] private AudioClip openSfx;
+    [SerializeField] private AudioClip closeSfx;
+    
+    private AudioSource _audio;
     private bool _isOpen;
+    
     private void Awake()
     {
         if (animator == null) animator = GetComponentInChildren<Animator>();
+        _audio = GetComponent<AudioSource>();
     }
     
     public void Open() => Set(true, defaultSpeed);
@@ -27,5 +33,12 @@ public class DoorController : MonoBehaviour
         _isOpen = state;
         animator.speed = Mathf.Max(0, speed);
         animator.SetBool(openParam, state);
+        PlaySfx();
+    }
+
+    private void PlaySfx()
+    {
+        var clip = _isOpen ? openSfx : closeSfx;
+        _audio.PlayOneShot(clip);
     }
 }
