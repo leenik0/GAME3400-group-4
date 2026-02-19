0a473c3664676f001d3d16d2b88c89426ce47166
Nicole Vafiadis <nicoleh.vafiadis@gmail.com>
2026-02-18 22:22:17 -0500

    Added countdown

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float timeRemaining = 900; // 15 minutes
+    private bool timerIsRunning = false;
+
+    private void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                DisplayTime(0);
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
