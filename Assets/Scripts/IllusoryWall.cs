85282e23f10f5bf49b2b7fa4b6bad6a9b48a653b
Sorano7 <sorano27@outlook.com>
2026-02-25 14:08:39 -0500

    Add script for disabling the wall based on camera direction

---
+++ Assets/Scripts/IllusoryWall.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class IllusoryWall : MonoBehaviour
+{
+    public Camera cam;
+    public Vector3 visibleSide = Vector3.forward;
+
+    private Renderer[] _renderers;
+
+    private void Awake()
+    {
+        _renderers = GetComponentsInChildren<Renderer>();
+        if (!cam) cam = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        if (!cam) return;
+
+        var pos = transform.position;
+        var n = transform.TransformDirection(visibleSide).normalized;
+        var side = Vector3.Dot(cam.transform.position - pos, n);
+
+        foreach (var r in _renderers) r.enabled = side > 0.01f;
+    }
+}
