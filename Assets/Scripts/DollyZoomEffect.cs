bf3ccc66f825626c07d97eb334ff0ccfb32615c0
Nicole Vafiadis <nicoleh.vafiadis@gmail.com>
2026-02-25 18:30:16 -0500

    Adjusted vignette and added motion blur

--- Assets/Scripts/DollyZoomEffect.cs
+++ Assets/Scripts/DollyZoomEffect.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class DollyZoomEffect : MonoBehaviour
 {
+    [Header("Zoom Details")]
     public Transform target; // object at end of hallway
     private Camera cam;
     private float initialHeight;
@@ -11,12 +14,37 @@ public class DollyZoomEffect : MonoBehaviour
     private float defaultFOV;
     private float targetFOV;
 
+    [Header("Vignette Effects")]
+    public Volume postProcessVolume;
+    public float maxIntensity = 0.5f;
+    public float increaseSpeed = 0.1f;
+    private Vignette vignette;
+
     void Start()
     {
         cam = GetComponent<Camera>();
         defaultFOV = cam.fieldOfView;
         float distance = Vector3.Distance(transform.position, target.position);
         initialHeight = 2.0f * distance * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        if (postProcessVolume.profile.TryGet(out vignette))
+        {
+            vignette.intensity.overrideState = true;
+            vignette.intensity.value = 0.2f;
+        }
+    }
+
+    void Update()
+    {
+        if (effectEnabled && target != null)
+        {
+            vignette.intensity.value += increaseSpeed * Time.deltaTime;
+            vignette.intensity.value = Mathf.Clamp(vignette.intensity.value, 0.2f, maxIntensity);
+        }
+        else if (vignette.intensity.value > 0.2f)
+        {
+            vignette.intensity.value -= increaseSpeed * Time.deltaTime;
+            vignette.intensity.value = Mathf.Clamp(vignette.intensity.value, 0.2f, maxIntensity);
+        }
     }
 
     void LateUpdate()
@@ -29,6 +57,7 @@ public class DollyZoomEffect : MonoBehaviour
             targetFOV = Mathf.Clamp(2.0f * Mathf.Atan(initialHeight * 0.5f * multiplier / currentDistance)
             * Mathf.Rad2Deg, 1f, 179f);
             cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, Time.deltaTime * transitionSpeed);
+
         }
         else
         {
